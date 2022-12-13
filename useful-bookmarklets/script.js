54ca02903eca505d7af6d261546b149f233c542e
Xavi Esteve <xavi@xaviesteve.com>
2022-12-13 20:15:22 +0100

    🌓 Dark theme

--- useful-bookmarklets/script.js
+++ useful-bookmarklets/script.js
@@ -478,6 +478,37 @@ const db = {
             css: 'heading',
         },
         {
+            title: '🌓 Dark theme',
+            css: 'default',
+            credit: 'https://sanketmehta7.github.io/DarkMode-JS-BookMark/',
+            description: `Rotate the website 90 degrees clockwise, keep clicking to rotate it further`,
+            func: function () {
+                //invert color and skip transparency value
+                function invert(str,type){
+                    var t1 = str.split("(");
+                    var t2 = t1[1].split(")");
+                    var t3 = t2[0].split(",");
+                    t3.forEach(function(v,i){
+                    if(i<3){
+                    if(type=="color")
+                        t3[i] = (255 - parseInt(v))<50?120:(255 - parseInt(v));
+                    else
+                        t3[i] = (255 - parseInt(v));
+                    }
+                    })
+                    t3 = t3.join(",");
+                    return t1[0]+"("+t3+")";
+                }
+                //invert color and backgroundcolor of every dom node
+                document.querySelectorAll('*:not([invTouch])').forEach(function(node) {
+                    var style  = window.getComputedStyle(node);
+                    node.style.backgroundColor = invert(style.backgroundColor,"back");
+                    node.style.color = invert(style.color,"color");
+                    node.setAttribute("invTouch", "true");
+                });
+            }
+        },
+        {
             title: '🔃 Rotate screen',
             css: 'default',
             description: `Rotate the website 90 degrees clockwise, keep clicking to rotate it further`,
